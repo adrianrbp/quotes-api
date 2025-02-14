72c7a0b595d775bd12b64fed5854485268a458c7
adrianrbp <eadrian.peralta@gmail.com>
2025-02-14 13:11:24 +0100

    feat: #2 define view, model and serializers

--- apps/quotes/models.py
+++ apps/quotes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Quote(models.Model):
+    id = models.AutoField(primary_key=True)
+    content = models.TextField()
+    author = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.content[:50]} - {self.author}"
--- apps/quotes/views.py
+++ apps/quotes/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
-from rest_framework.response import Response
+from apps.quotes.models import Quote
+from apps.quotes.serializers import QuoteSerializer
 
-class QuoteListCreateView(generics.GenericAPIView):
-    def get(self, request):
-        return Response([])
+class QuoteListCreateView(generics.ListCreateAPIView):
+    queryset = Quote.objects.all()
+    serializer_class = QuoteSerializer
