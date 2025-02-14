37928ecc7527286a9d6e46a1f8f082cc91061c73
adrianrbp <eadrian.peralta@gmail.com>
2025-02-14 14:37:19 +0100

    doc: #2 add documentation to view, model and serializer

--- apps/quotes/serializers.py
+++ apps/quotes/serializers.py
@@ -2,7 +2,11 @@ from rest_framework import serializers
 from apps.quotes.models import Quote
 
 class QuoteSerializer(serializers.ModelSerializer):
-
+    """
+    Serializer for the Quote model.
+    Transforms Quote instances into JSON and validates input data for API requests.
+    """
+    
     class Meta:
         model = Quote
         # fields = '__all__'
--- apps/quotes/views.py
+++ apps/quotes/views.py
@@ -3,5 +3,11 @@ from apps.quotes.models import Quote
 from apps.quotes.serializers import QuoteSerializer
 
 class QuoteListCreateView(generics.ListCreateAPIView):
+    """
+    API endpoint that allows quotes to be viewed.
+
+    Endpoints:
+    - GET /api/quotes/       -> Retrieve all quotes
+    """
     queryset = Quote.objects.all()
     serializer_class = QuoteSerializer
