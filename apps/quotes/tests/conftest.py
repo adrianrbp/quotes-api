ef510582d5fd903b89451e5f1d329abd960d3bcb
adrianrbp <eadrian.peralta@gmail.com>
2025-02-14 12:59:21 +0100

    feat: #2 extend list endpoint to retrieve quotes

--- apps/quotes/tests/conftest.py
+++ apps/quotes/tests/conftest.py
@@ -1,6 +1,16 @@
 import pytest
 from rest_framework.test import APIClient
+from apps.quotes.models import Quote
 
 @pytest.fixture
 def api_client():
     return APIClient()
+
+
+@pytest.fixture
+def quote(db):
+    """Creates a test quote"""
+    return Quote.objects.create(
+            content="The only limit to our realization of tomorrow is our doubts of today.",
+            author="Franklin D. Roosevelt"
+        )
--- apps/quotes/tests/test_api.py
+++ apps/quotes/tests/test_api.py
@@ -3,8 +3,10 @@ from rest_framework import status
 
 @pytest.mark.django_db
 class TestQuoteAPI:
-    def test_list_quotes(self, api_client):
+    def test_list_quotes(self, api_client, quote):
         """Ensure we can retrieve a list of quotes"""
         response = api_client.get("/api/quotes/")
         assert response.status_code == status.HTTP_200_OK
-        assert response.data == []
+        assert len(response.data) == 1
+        assert response.data[0]["content"] == quote.content
+        assert response.data[0]["author"] == quote.author
