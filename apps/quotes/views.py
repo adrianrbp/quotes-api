6a6bc073c14680d3dc6e6e3ed7ef11c8b50ff074
adrianrbp <eadrian.peralta@gmail.com>
2025-02-16 16:56:22 +0100

    refactor: #3 add responses utils for openapi structure

--- apps/quotes/views.py
+++ apps/quotes/views.py
@@ -1,9 +1,9 @@
 import random
 from rest_framework.decorators import action
-from rest_framework.response import Response
 from rest_framework import viewsets, status
 from apps.quotes.models import Quote
 from apps.quotes.serializers import QuoteSerializer
+from apps.quotes.utils.responses import api_response
 
 class QuoteViewSet(viewsets.ModelViewSet):
     """
@@ -29,23 +29,9 @@ class QuoteViewSet(viewsets.ModelViewSet):
         quotes = Quote.objects.all()
 
         if not quotes.exists():
-            return Response(
-                {
-                    "status": 404,
-                    "message": "No quotes available",
-                    "data": None
-                },
-                status=status.HTTP_404_NOT_FOUND
-            )
+            return api_response(status.HTTP_404_NOT_FOUND, "No quotes available")
 
         random_quote = random.choice(quotes)
         serializer = self.get_serializer(random_quote)
 
-        return Response(
-            {
-                "status": 200,
-                "message": "Random quote retrieved successfully",
-                "data": serializer.data
-            },
-            status=status.HTTP_200_OK
-        )
+        return api_response(status.HTTP_200_OK, "Random quote retrieved successfully", serializer.data)
